aaef323244df021eb66b6e60045e5f8e0aceaae0
Julia Silva <juliasilvass01@gmail.com>
2024-02-28 21:09:12 -0300

    Primeira aula

---
+++ Aula 1/exemplo2.java
@@ -0,0 +1,10 @@
+public class exemplo2 {
+    public static void main(String args[]){
+        String nome = "Julia";
+        int codigo = 1020; 
+        float salario = 500.00f;
+
+        System.out.println("Seu nome é: " + nome + " e o código é: " + codigo);
+        System.out.printf("Seu salario: RS %.2f ", salario);
+    }
+}
